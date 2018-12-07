79cd06b2f11ad793787753b3d3f4ef6f51e975f2
Noah A <nanderson815@gmail.com>
2018-12-07 15:23:06 -0500

    Updated 12 8 18

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var fighters = [
+        Boba = {
+            name: "Boba Fett",
+            healthPoints: "150",
+            attackPower: "11",
+            counterAttackPower: "12"
+        },
+        Darth = {
+            name: "Darth Vader",
+            healthPoints: "200",
+            attackPower: "12",
+            counterAttackPower: "15"
+        },
+        R2D2 = {
+            name: "R2-D2",
+            healthPoints: "125",
+            attackPower: "10",
+            counterAttackPower: "17"
+        },
+        clone = {
+            name: "Clone Soldier",
+            healthPoints: "150",
+            attackPower: "15",
+            counterAttackPower: "13"
+        }
+    ]
+
+    console.log(fighters[0]);
+
+
+
+});
